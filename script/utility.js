7261fd76236727ee7ac17d947ad5a0e808f6acee
rafih188 <rafeh1888@gmail.com>
2024-02-22 17:25:54 +0600

    added more seat

--- script/utility.js
+++ script/utility.js
@@ -1,1 +1,11 @@
-console.log('this is utility js');
+const aone = document.getElementById('aone').childNodes;
+console.log(aone);
+for (a of aone){
+    a.addEventListener('click', changeBgColor)
+}
+
+function changeBgColor(){
+    let seat = document.getElementById('a');
+    seat.style.background = '#1DD100';
+
+}
