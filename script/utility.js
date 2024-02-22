267e8e21cfdee588e1758841be5ba20e8cb78e60
rafih188 <rafeh1888@gmail.com>
2024-02-22 22:59:54 +0600

    last try

--- script/utility.js
+++ script/utility.js
@@ -1,11 +1,48 @@
-const aone = document.getElementById('aone').childNodes;
-console.log(aone);
-for (a of aone){
-    a.addEventListener('click', changeBgColor)
+
+
+const allButton = document.getElementsByClassName("w-1/5");
+
+for(let i = 0 ; i<allButton.length; i++){
+const button = allButton[i];
+button.addEventListener('click',function(){
+    console.log('clicked');
+    button.classList.add('jsButton');
+    const seatName = button.innerText;
+    // show seat name in right side ===
+    const spanSeatName = document.getElementById('seat-name').innerText = seatName;
+
+    // (- total seat)===
+    let totalSeat = parseInt(document.getElementById("total-seat").innerText);
+    let updatedTotalSeat =  totalSeat -=1 ;
+    // totalSeat= updatedTotalSeat;
+
+    let changedTotalSeat =document.getElementById("total-seat").innerText= updatedTotalSeat;
+
+ 
+  
+    // booked seat here ===
+
+    let bookedSeat =parseInt( document.getElementById('booked-seat').innerText);
+let updatedBookedSeat = bookedSeat += 1;
+let changedBookedSeat = document.getElementById('booked-seat').innerText=updatedBookedSeat;
+
+if(bookedSeat === 5){
+    alert('do not book any more seat')
+    button.classList.remove('jsButton')
+
 }
 
-function changeBgColor(){
-    let seat = document.getElementById('a');
-    seat.style.background = '#1DD100';
+
+    
+
+
+
+    
+
+})
 
 }
+
+
+
+
